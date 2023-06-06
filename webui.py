dd24622f47e5d86af5589debeafcf705b107af2e
Your Name <you@example.com>
2023-06-06 11:50:07 +0100

    some proto webui attempts9

---
+++ webui.py
@@ -0,0 +1,51 @@
+from flask import Flask, jsonify, request, send_from_directory
+import os
+import json
+
+app = Flask(__name__)
+
+# Home page that serves the index.html file from the 'webserver' directory
+@app.route('/')
+def home():
+    return send_from_directory('static', 'index.html')
+
+# API to get the file structure of midi_lib recursively
+@app.route('/api/midi_lib', methods=['GET'])
+def midi_lib_structure():
+    def fetch_structure(path):
+        tree = {}
+        for item in os.scandir(path):
+            if item.is_dir():
+                tree[item.name] = fetch_structure(item.path)
+            else:
+                tree[item.name] = item.path
+        return tree
+
+    structure = fetch_structure('midi_lib')
+    return jsonify(structure)
+
+# API to list files in songs_lib
+@app.route('/api/songs_lib', methods=['GET'])
+def songs_lib():
+    if not os.path.isdir('songs_lib'):
+        return jsonify({'error': 'The specified path does not exist or is not a directory'}), 404
+
+    content = os.listdir('songs_lib')
+    return jsonify(content)
+
+# API to save JSON file to songs_lib
+@app.route('/api/songs_lib', methods=['POST'])
+def save_to_songs_lib():
+    data = request.json
+    filename = request.args.get('filename', default = '', type = str)
+
+    if filename == '':
+        return jsonify({'error': 'You must specify a filename'}), 400
+
+    with open(os.path.join('songs_lib', filename), 'w') as f:
+        json.dump(data, f)
+
+    return jsonify({'message': 'File saved successfully'})
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
